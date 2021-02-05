8c41fb676e643c2c68836c5dde8ee5b8737c6a54
Paul Chávez <paulchavezromero20@gmail.com>
2021-02-05 15:26:46 -0500

    Header y CSS added

---
+++ pages/crear-cuenta.js
@@ -0,0 +1,13 @@
+import Layout from '../components/layout/Layout';
+
+const CrearCuenta = () => {
+    return (
+        <div>
+            <Layout>
+                <h1>Crear cuenta</h1>
+            </Layout>
+        </div>
+    )
+}
+
+export default CrearCuenta;
