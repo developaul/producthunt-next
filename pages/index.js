a953c8c83813702eed3515840865b255bfbf209c
Paul Chávez <paulchavezromero20@gmail.com>
2021-02-15 13:45:09 -0500

    products on the main page added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,48 @@
-import styled from '@emotion/styled';
+import React, { useEffect, useState, useContext } from 'react';
+
 import Layout from '../components/layout/Layout';
+import DetallesProducto from '../components/layout/DetallesProducto';
+import { FirebaseContext } from '../firebase';
 
 const Home = () => {
+
+	const [productos, setProductos] = useState([]);
+
+	const { firebase } = useContext(FirebaseContext);
+
+	// Permite Obtener todos los productos
+	useEffect(() => {
+
+		(async () => {
+
+			await firebase.db.collection('productos').orderBy('creado', 'desc').onSnapshot(handleSnapshot)
+
+		})();
+
+	}, []);
+
+	function handleSnapshot(snapshot) {
+		const productos = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+		setProductos(productos);
+	}
+
 	return (
 		<div>
 			<Layout>
-				<h1>Inicio</h1>
+				<div className="listado-productos">
+					<div className="contenedor">
+						<div className="bg-white">
+							{
+								productos.map(producto => (
+									<DetallesProducto
+										key={producto.id}
+										{...producto}
+									/>
+								))
+							}
+						</div>
+					</div>
+				</div>
 			</Layout>
 		</div>
 	)
